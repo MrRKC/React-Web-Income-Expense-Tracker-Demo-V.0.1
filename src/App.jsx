51e6c136aac6bff8371d3ad61a631ae993140d5d
MrRKC <apamrrkc@gmail.com>
2024-01-10 06:52:30 +0700

    new Up

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,6 @@ import Header from './components/Header/Header';
 import Transaction from './components/Transaction/Transaction';
 import ContextHandler from './context/Context'
 
-// const INITAL_EXPENSE = [
-//     {
-//         id: 1,
-//         task: "Pizza",
-//         amount: -150,
-//         payment:"Cash",
-//         ExIn: false, // true = Income, false = Expenses
-//         dueDate: new Date("2023-02-28"),
-//     },
-//     {
-//         id: 2,
-//         task: "Salary",
-//         amount: 30000,
-//         payment:"",
-//         ExIn: true,
-//         dueDate: new Date("2024-06-14"),
-//     },
-//     {
-//         id: 3,
-//         task: "Book",
-//         amount: -300,
-//         payment:"Credit Card",
-//         ExIn: false,
-//         dueDate: new Date("2023-05-20"),
-//     },
-//     {
-//         id: 4,
-//         task: "Camera",
-//         amount: -1200,
-//         payment:"Bank App",
-//         ExIn: false,
-//         dueDate: new Date("2024-07-26"),
-//     },
-// ];
-
 const App = () => {
     return (
         <ContextHandler>
